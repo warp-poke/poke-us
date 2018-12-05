5d84328eb78104c1c05e856b639d96ebc6d14c85
aurrelhebert <aurrelhebert@gmail.com>
2018-12-05 18:00:25 +0100

    feat(Attributes): add check on series Attributes

--- app/models/warpscripts/Checks.scala
+++ app/models/warpscripts/Checks.scala
@@ -8,17 +8,34 @@ object Checks {
     List(
       query.fetch,
       query.filterLast(op, value),
+      query.notQuiesce,
+      query.checkLastNotify,
       query.reduce(labels),
       query.footer
     ).mkString("")
   }
 
-  def delta(token: String, selector: String, duration: String, range: Integer, value: String, op: String, labels: Option[String] = None): String = {
+  def deltaLastValue(token: String, selector: String, duration: String, range: Integer, value: String, op: String, labels: Option[String] = None): String = {
     val query = Query(token, selector, duration)
     List(
       query.fetch,
       query.delta(range),
       query.filterLast(op, value),
+      query.notQuiesce,
+      query.checkLastNotify,
+      query.reduce(labels),
+      query.footer
+    ).mkString("\n")
+  }
+
+  def deltaMapper(token: String, selector: String, duration: String, range: Integer, value: String, op: String, labels: Option[String] = None): String = {
+    val query = Query(token, selector, duration)
+    List(
+      query.fetch,
+      query.delta(range),
+      query.mapperLast(op, value),
+      query.notQuiesce,
+      query.checkLastNotify,
       query.reduce(labels),
       query.footer
     ).mkString("\n")
